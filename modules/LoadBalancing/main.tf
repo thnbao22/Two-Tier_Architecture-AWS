a76fca868d081b95b725d077c529d7b87cf5b8c0
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-06-29 00:53:39 +0700

    modify main.tf module LoadBalancing

--- modules/LoadBalancing/main.tf
+++ modules/LoadBalancing/main.tf
@@ -2,21 +2,21 @@ resource "aws_lb" "two_tier_lb" {
   name                  = "${var.resource_name}-lb"
   internal              = false
   load_balancer_type    = "application"
-  security_groups       = [ "${var.elb_sg_id}" ]
-  subnets               = [ "${var.public_subnet}" ]
+  security_groups       = [var.elb_sg_id]
+  subnets               = [var.public_subnet]
 }
 
 resource "aws_lb_target_group" "two_tier_tg" {
   name      = "${var.resource_name}-tg"
-  port      = "${var.http_port}"
-  protocol  = "${var.protocol}"
-  vpc_id    = "${var.vpc_id}"
+  port      = var.http_port
+  protocol  = var.protocol
+  vpc_id    = var.vpc_id
 }
 
 resource "aws_lb_listener" "two_tier_lb_listener" {
-  load_balancer_arn = "${aws_lb.two_tier_lb.arn}"
-  port              = "${var.http_port}"
-  protocol          = "${var.protocol}"
+  load_balancer_arn = aws_lb.two_tier_lb.arn
+  port              = var.http_port
+  protocol          = var.protocol
   default_action {
     type                = "forward"
     target_group_arn    = aws_lb_target_group.two_tier_tg.arn
