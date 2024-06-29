af37df6371335df385ba46b7f2776f79af1943d7
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-06-29 15:23:07 +0700

    Add main.tf in folder terraform

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+locals {
+  resource_name = "two-tier"
+  instance_type = "t2.micro"
+  http_port = 80
+}
+
+module "Networking" {
+  source                = "../modules/Networking"
+  vpc_cidr_block        = ""
+  resource_name         = local.resource_name
+  public_subnet_count   = 2
+  private_subnet_count  = 2
+  ssh_port              = 22
+  http_port             = local.http_port
+  postgre_port          = 5432
+}
+
+module "Compute" {
+  source        = "../modules/Compute"
+  resource_name = local.resource_name
+  instance_type = local.instance_type
+  asg_sg_id     = module.Networking.two_tier_asg_sg_id
+  elb_tg_arn    = module.LoadBalancing.elb_tg_arn
+  public_subnet = module.Networking.public_subnet_ids
+}
+
+module "LoadBalancing" {
+  source        = "../modules/LoadBalancing"
+  resource_name = local.resource_name
+  elb_sg_id     = module.Networking.elb_sg_id
+  vpc_id        = module.Networking.vpc_id
+  public_subnet = module.Networking.public_subnet_ids
+  http_port     = local.http_port
+  protocol      = "HTTP"
+}
+
+module "Database" {
+  source                = "../modules/database"
+  resource_name         = local.resource_name
+  db_allocated_storage  = 20
+  engine                = "postgres"
+  engine_version        = "16.4"
+  db_instance_class     = "db.t2.micro"
+  db_port               = 5432
+  db_subnet_group_name  = module.Networking.rds_subnet_group_name
+  username              = local.resource_name
+  password              = local.resource_name
+}
